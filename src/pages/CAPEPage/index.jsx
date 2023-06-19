62f4a676a2c7502441169428706b41dbe6396776
Jacob Shalabi <70823266+jshalabi03@users.noreply.github.com>
2023-06-18 22:03:03 -0500

    Added week3 button

--- src/pages/CAPEPage/index.jsx
+++ src/pages/CAPEPage/index.jsx
@@ -56,6 +56,7 @@ const CAPEPage = () => {
                 <MenuItem _hover={{ bg: '#FDC0C0', borderRadius: "5px"}} bgColor={"#f0a89e"} padding={"10px"} onClick={() => setMenuLabel("Week 0")}>Week 0</MenuItem>
                 <MenuItem _hover={{ bg: '#FDC0C0', borderRadius: "5px"}} bgColor={"#f0a89e"} padding={"10px"} onClick={() => setMenuLabel("Week 1")}>Week 1</MenuItem>
                 <MenuItem _hover={{ bg: '#FDC0C0', borderRadius: "5px"}} bgColor={"#f0a89e"} padding={"10px"} onClick={() => setMenuLabel("Week 2")}>Week 2</MenuItem>
+                <MenuItem _hover={{ bg: '#FDC0C0', borderRadius: "5px"}} bgColor={"#f0a89e"} padding={"10px"} onClick={() => setMenuLabel("Week 3")}>Week 3</MenuItem>
               </MenuList>
             </Portal>  
             </Menu>
